2ee0dc4b5ec1e288b09b5c1390df143ed5f41a90
jmarsico <jmarsico@gmail.com>
2013-12-09 19:39:59 -0500

    added noise function and noise array, added steven's law (not working)

--- src/testApp.cpp
+++ src/testApp.cpp
@@ -1,25 +1,24 @@
 #include "testApp.h"
 
-//Often I have to run 
-// $sudo amixer cset numid=3 1
-//
-
 
 //--------------------------------------------------------------
-void testApp::setup(){
-	//ofSetFrameRate(15);
+void testApp::setup()
+{
 	
+/*
 	//construct new PCA9685 object with the number of boards you're using
 	numBoards = 2;
-	//pca = new PCA9685(numBoards);
+	pca = new PCA9685(numBoards);
 
-
-	ofSetVerticalSync(false);
+*/
+	
+    ofSetVerticalSync(false);
 	cameraWidth		= 320;
 	cameraHeight	= 240;
 	cellSize = 48;
 	cellSizeFl  = (float)cellSize;
 	numPixels = (cameraWidth/cellSize) * (cameraHeight/cellSize);
+    ofLog() << "numPixels: " << numPixels;
 
 	displayCoeff = 1;
 	
@@ -28,40 +27,21 @@ void testApp::setup(){
 	videoGrabber.initGrabber(cameraWidth, cameraHeight);
 	pixels = new unsigned char[numPixels];
 
-    //pixelTexture.allocate(cameraWidth, cameraHeight, GL_LUMINANCE);
+    noiseSpeedX = 0.005;
+    noiseSpeedY = 0.005;
+    noiseAmp = 125;
+    
+    time = 0;
 
 
 }
 
 
 //--------------------------------------------------------------
-void testApp::update(){
-
-	/////////////TEST BLINK FIRST LED/////////////////
-/*
-	pca->setLED(0,4095);
-	pca->setLED(16, 4095);
-	sleep(1);
-
-	pca->setLED(0,1);
-	pca->setLED(16, 1);
-	sleep(1);
-*/
+void testApp::update()
+{
 	
-
-	///////////// RAMP TEST ////////////////////
-    
-/*	
-	for(int val = 1; val < 4096; val+=100)
-	{
-		for(int i = 0; i < 16*numBoards; i++)
-		{
-			pca->setLED(i, val);
-
-		}
-	}
-*/		
-	//////////////////Prod Section //////////////////
+//////////////////Prod Section //////////////////
 	
 	
 	videoGrabber.update();
@@ -78,17 +58,40 @@ void testApp::update(){
 			{
 				int x = i*cellSize;
 				int y = j*cellSize;
-				br[(j*cameraWidth/cellSize)+i] = (float)pix.getColor(x,y).getLightness();
+				br[(j*(cameraWidth/cellSize))+i] = (float)pix.getColor(x,y).getLightness();
 			}
         }
     }
 
 
     //runLights(br);
+    makeNoise();
+}
+
+void testApp::makeNoise(void)
+{
+    for(int i = 0; i < 30; i++)
+    {
+        noiseVal[i] = noiseAmp * ofNoise(time * noiseSpeedX*(i+10), time*noiseSpeedY*(10-i));
+        
+    }
+    time = ofGetElapsedTimef();
+}
+
+float testApp::stevensLaw(float val)
+{
+    float newVal;
+    if(val <= 255)
+    {
+       newVal = pow(255.0 * ((float)val/(float)255), 1.0/0.5) + 0.5;
+    }
+    
+    return newVal;
 }
 
 //--------------------------------------------------------------
 /*
+ 
 void testApp::runLights(float br[])
 {
 	int lightBright[16*numBoards];
@@ -101,7 +104,8 @@ void testApp::runLights(float br[])
     }
 }
 
-	*/
+*/
+
 
 //--------------------------------------------------------------
 void testApp::draw(){
@@ -124,17 +128,18 @@ void testApp::draw(){
 	
 	ofDrawBitmapString("Pixelated and Mirrored", cameraWidth*displayCoeff+leftSpace+space+cellSize, height - 20);
 
-	for(int j = 0; j < cameraHeight/cellSize; j++)
+	for(int j = 0; j < (cameraHeight/cellSize); j++)
 		{
-			for(int i = 0; i < cameraWidth/cellSize; i++)
+			for(int i = 0; i < (cameraWidth/cellSize); i++)
 			{
 				ofPushMatrix();
 				ofTranslate(space,0);
 				ofTranslate(i*cellSize*displayCoeff+cameraWidth*displayCoeff+space, j*cellSize*displayCoeff+height);
-					ofSetColor(br[j*cameraWidth/cellSize+i]);
+				//ofSetColor(stevensLaw(br[j*(cameraWidth/cellSize)+i]));
+                //ofSetColor((noiseVal[j*(cameraWidth/cellSize)+i]) + ofMap(br[j*(cameraWidth/cellSize)+i], 0, 255, 0, 125));
 					ofRect(0.0,0.0,cellSizeFl*displayCoeff, cellSizeFl*displayCoeff);
                     ofSetColor(255,0,0);
-                    ofDrawBitmapString(ofToString(j*cameraWidth/cellSize+i), 0,10);
+                    ofDrawBitmapString(ofToString(j*(cameraWidth/cellSize)+i), 0,10);
 				ofPopMatrix();
 			}
 		}
@@ -145,4 +150,35 @@ void testApp::draw(){
 	ofDrawBitmapString("FPS: " + ofToString(ofGetFrameRate(), 0), 5, ofGetWindowHeight()-5);
     
 }
+
+
+void testApp::testLights(void)
+{
+    
+	/////////////TEST BLINK FIRST LED/////////////////
+    /*
+     pca->setLED(0,4095);
+     pca->setLED(16, 4095);
+     sleep(1);
+     
+     pca->setLED(0,1);
+     pca->setLED(16, 1);
+     sleep(1);
+     */
+	
+    
+	///////////// RAMP TEST ////////////////////
+    
+    /*
+     for(int val = 1; val < 4096; val+=100)
+     {
+     for(int i = 0; i < 16*numBoards; i++)
+     {
+     pca->setLED(i, val);
+     
+     }
+     }
+     */
+}
+
 
--- src/testApp.h
+++ src/testApp.h
@@ -1,7 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
-#include "PCA9685.h"
+//#include "PCA9685.h"
 
  // set to -1 to not use the enable pin (its optional)
 
@@ -13,12 +13,17 @@ public:
 	void update();
 	void draw();
 	void runLights(float br[]);
+    void makeNoise(void);
+    float stevensLaw(float val);
+    
+    void testLights(void);
 
 	int val;
 	int valInc;
 
 
-	float br[48];
+	float br[30];
+    float noiseVal[30];
 	unsigned char* pixels;
 	int cellSize;
 	float cellSizeFl;
@@ -28,13 +33,15 @@ public:
 	int	cameraHeight;
 
 	float displayCoeff;
+    
+    float noiseSpeedX, noiseSpeedY, noiseAmp, time;
 
 	
 	int numBoards;
 
 	ofVideoGrabber videoGrabber;
 	//ofTexture pixelTexture;
-	PCA9685* pca; //initialize the class with number of boards
+//	PCA9685* pca; //initialize the class with number of boards
 
 
 
